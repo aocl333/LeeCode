48e8e8e1d37dcdbe6f3d53f89dadff8e277d33d9
Ally <56382506+aocl333@users.noreply.github.com>
2022-07-09 23:10:00 +0900

    Time: 279 ms (46.54%), Space: 51.5 MB (22.69%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let palindrome = String(x).split("").reverse().join("");
+    return String(x) === palindrome;
+    
+};
