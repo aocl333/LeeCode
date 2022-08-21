770e3870ec4672a3aa74ccd645a4658960741dce
Ally <56382506+aocl333@users.noreply.github.com>
2022-08-21 22:43:34 +0900

    Time: 89 ms (73.39%), Space: 44.8 MB (36.38%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    return symmetric(root,root)
+    
+    function symmetric(treeLeft, treeRight) {
+        if(treeLeft===null && treeRight===null) 
+        {return true};
+        if(treeLeft===null || treeRight===null) 
+        {return false};
+        return (treeLeft.val===treeRight.val) && symmetric(treeLeft.right,treeRight.left) && symmetric(treeLeft.left,treeRight.right)
+    }
+};
